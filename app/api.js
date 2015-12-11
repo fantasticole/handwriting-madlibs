ad5f47816a5d9324d1e48a3c1ff021dac293a044
FantastiCole <coleamurphy@gmail.com>
2015-12-11 11:14:54 -0500

    Get API working and render first image.

--- app/api.js
+++ app/api.js
@@ -1,17 +1,18 @@
 import request from 'browser-request'
 import {tokens} from 'config'
 
-export function getHandwriting(text, cb){	
+export function getHandwriting(text, cb){
 	let parameters = {
 		url: 'render/png?',
-		Authorization: tokens.KEY+':'+tokens.SECRET,
 		handwriting_id: '31SB2CWG00DZ',
+		handwriting_size: '16px',
+		line_spacing: 1,
 		width: '200px',
-		height: '200px',
+		height: '200px'
 	}
-	console.log(parameters)
+	// console.log(parameters)
 
-	return request({
+	request({
 		method: 'GET',
 		uri: 'https://api.handwriting.io/handwritings',
 		auth: {
@@ -21,10 +22,14 @@ export function getHandwriting(text, cb){
 	}, function (error, response, body) {
 		console.log(response.statusCode)
 	  if (!error && response.statusCode == 200) {
-	    console.log(body) // Show the HTML for the page.
-	    cb(JSON.parse(body))
+	    console.log('Good to go!') // Confirm receipt of status: OK
+		let img = document.createElement('img')
+	    img.src = 'https://'+tokens.KEY+':'+tokens.SECRET+'@api.handwriting.io/render/png?handwriting_id='+parameters.handwriting_id+'&handwriting_size='+parameters.handwriting_size+'&line_spacing='+parameters.line_spacing+'&handwriting_color=%23000000&width='+parameters.width+'&height='+parameters.height+'&text='+text
+	    // img.src = 'https://'+tokens.KEY+':'+tokens.SECRET+'@api.handwriting.io/render/png?handwriting_id='+parameters.handwriting_id+'&text=Test+For+Cole&handwriting_size=20px&width=500px&height=auto'
+	    cb(img)
 	  }
 	})
+
 }
 
 
--- static/bundle.js
+++ static/bundle.js
@@ -20255,14 +20255,15 @@
 	function getHandwriting(text, cb) {
 		var parameters = {
 			url: 'render/png?',
-			Authorization: _config.tokens.KEY + ':' + _config.tokens.SECRET,
 			handwriting_id: '31SB2CWG00DZ',
+			handwriting_size: '16px',
+			line_spacing: 1,
 			width: '200px',
 			height: '200px'
 		};
-		console.log(parameters);
+		// console.log(parameters)
 
-		return (0, _browserRequest2['default'])({
+		(0, _browserRequest2['default'])({
 			method: 'GET',
 			uri: 'https://api.handwriting.io/handwritings',
 			auth: {
@@ -20272,8 +20273,11 @@
 		}, function (error, response, body) {
 			console.log(response.statusCode);
 			if (!error && response.statusCode == 200) {
-				console.log(body); // Show the HTML for the page.
-				cb(JSON.parse(body));
+				console.log('Good to go!'); // Confirm receipt of status: OK
+				var img = document.createElement('img');
+				img.src = 'https://' + _config.tokens.KEY + ':' + _config.tokens.SECRET + '@api.handwriting.io/render/png?handwriting_id=' + parameters.handwriting_id + '&handwriting_size=' + parameters.handwriting_size + '&line_spacing=' + parameters.line_spacing + '&handwriting_color=%23000000&width=' + parameters.width + '&height=' + parameters.height + '&text=' + text;
+				// img.src = 'https://'+tokens.KEY+':'+tokens.SECRET+'@api.handwriting.io/render/png?handwriting_id='+parameters.handwriting_id+'&text=Test+For+Cole&handwriting_size=20px&width=500px&height=auto'
+				cb(img);
 			}
 		});
 	}
